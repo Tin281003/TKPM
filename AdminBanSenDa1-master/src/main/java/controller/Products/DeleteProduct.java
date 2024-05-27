88ea7ba1d12335a481fc07a0a386107500903df1
Lyly-126 <21130441@st.hcmuaf.edu.vn>
2024-05-27 14:07:09 +0700

    sua DB

---
+++ AdminBanSenDa1-master/src/main/java/controller/Products/DeleteProduct.java
@@ -0,0 +1,37 @@
+package controller.Products;
+
+import bean.User;
+import service.ProductService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet(name = "DeleteProduct", value = "/DeleteProduct")
+public class DeleteProduct extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String masp = request.getParameter("masp");
+    /*
+     6. Gọi đến hàm removeProduct(masp)
+    */
+        ProductService.removeProduct(masp);
+
+        HttpSession session = request.getSession(true);
+        User user = (User) session.getAttribute("auth");
+
+    /*
+    8. Hiển thị danh sách sản phẩm
+    */
+        response.sendRedirect("ListProduct_Admin");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
